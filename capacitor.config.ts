c73e318ce7a85c7f147309f64eb5604af10398b9
jojimenez4 <jo.jimenezh@duocuc.cl>
2022-08-31 16:10:21 -0400

    Inicio Proyecto

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CentroMec',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
