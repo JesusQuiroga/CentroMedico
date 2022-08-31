1816663f94a70ce7f09d068d19f2803f82816678
jojimenez4 <jo.jimenezh@duocuc.cl>
2022-08-31 16:14:58 -0400

    Pagina login agregada

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'CentroMec',
+  appName: 'CentroMedico',
   webDir: 'www',
   bundledWebRuntime: false
 };
